af8bb168e94d3bf15270ec1f577663a1a482a477
John Corser <johnpc@umich.edu>
2024-01-17 18:34:00 -0500

    a little formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 import { generateClient } from "aws-amplify/api";
 import Post from "@/components/post/Post";
-import { Image } from "@aws-amplify/ui-react";
+import { Image, Text } from "@aws-amplify/ui-react";
 const client = generateClient<Schema>();
 
 export type LoadedPost = {
@@ -45,28 +45,33 @@ export default function Home() {
   }, []);
   return (
     <>
-      <Grid
-        item
-        xs={12}
-        md={8}
-        sx={{
-          "& .markdown": {
-            py: 3,
-          },
-        }}
-      >
-        <Image width="100%" height="50%" alt="unsplash" src="https://source.unsplash.com/collection/94734566/1920x500" />
-        <Divider />
-        {posts.map((post) => (
-          <div key={post.id} style={{ padding: "5px" }}>
-            <Post
-              post={post as unknown as Schema["Post"]}
-              comments={post.comments}
-              showPostLink
-            />
-          </div>
-        ))}
-      </Grid>
+      <div style={{ width: "100%", height: "100%", minHeight: "300px", backgroundImage: "url(https://source.unsplash.com/collection/94734566/1920x500)" }}>
+        <Text variation="primary" as="h1" fontSize="5em" color="whitesmoke" style={{ textAlign: 'center', textShadow: '1px 1px 2px black', transform: "translateY(50%)"}}>Blog Title</Text>
+      </div>
+      <Divider />
+      <div style={{ width: "50%", marginLeft: "25%" }}>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          sx={{
+            "& .markdown": {
+              py: 3,
+            },
+          }}
+        >
+          {posts.map((post) => (
+            <div key={post.id} style={{ padding: "5px" }}>
+              <Post
+                post={post as unknown as Schema["Post"]}
+                comments={post.comments}
+                showPostLink
+              />
+            </div>
+          ))}
+        </Grid>
+      </div>
     </>
+
   );
 }
